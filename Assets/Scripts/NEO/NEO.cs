adc00f518eb727e4d5edb66d0d9626ad0a5715b8
petaaar88 <pe.bn80@gmail.com>
2025-09-13 15:35:59 +0200

    Add and setup sfx for NEOs

--- Assets/Scripts/NEO/NEO.cs
+++ Assets/Scripts/NEO/NEO.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 
 public class NEO : MonoBehaviour
 {
+
+    private ObjectAudioManager audioManager;
+
+
+    private void Start()
+    {
+        audioManager = GetComponent<ObjectAudioManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ground"))
         {
+
+            audioManager.PlaySound("Breaking");
+
             NoiceListener.Instance.RegisterLoudNoice(transform.position);
             foreach (var col in GetComponents<Collider>())
             {
